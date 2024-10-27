182022eba8049b0a5b6e3ccbdde639e1d46075ee
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-10-27 13:12:16 +0600

    All front is converted into java

--- src/main/java/com/example/FreshFarm/Delivery/controller/PageController.java
+++ src/main/java/com/example/FreshFarm/Delivery/controller/PageController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor
 @RequestMapping("/pages")
 public class PageController {
+
+    @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("title", "Fresh Farm Delivery");
         return "index";
@@ -27,5 +29,53 @@ public class PageController {
     public String register(Model model) {
         model.addAttribute("authRegisterRequest", new AuthRegisterRequest());
         return "register";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "blog";
+    }
+
+    @GetMapping("/blog-details")
+    public String blogDetails(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "blog-details";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "checkout";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "contact";
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "main";
+    }
+
+    @GetMapping("/shop-details")
+    public String shopDetails(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "shop-details";
+    }
+
+    @GetMapping("/shop-grid")
+    public String shopGrid(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "shop-grid";
+    }
+
+    @GetMapping("/shopping-cart")
+    public String shoppingCart(Model model) {
+        model.addAttribute("title", "Fresh Farm Delivery");
+        return "shoping-cart";
     }
 }
