574cb64801ab47c209c80064fece3f6516dd83ff
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2025-02-10 10:54:33 +0600

    fix: Links in front are fixed

--- src/main/java/com/example/FreshFarm/Delivery/controller/AuthController.java
+++ src/main/java/com/example/FreshFarm/Delivery/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.example.FreshFarm.Delivery.model.dto.auth.AuthLoginRequest;
 import com.example.FreshFarm.Delivery.model.dto.auth.AuthRegisterRequest;
 import com.example.FreshFarm.Delivery.model.dto.auth.AuthResponse;
 import com.example.FreshFarm.Delivery.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +17,29 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public String register(@ModelAttribute AuthRegisterRequest request) {
-        AuthResponse response = authService.register(request);
-        return "redirect:/pages/login";
+    public String register(@ModelAttribute AuthRegisterRequest request, HttpServletResponse response) {
+        AuthResponse authResponse = authService.register(request);
+        cookieOperation(authResponse, response);
+        return "redirect:/pages/home";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute AuthLoginRequest request) {
-        AuthResponse response = authService.login(request);
-        return "redirect:/pages/register";
+    public String login(@ModelAttribute AuthLoginRequest request, HttpServletResponse response) {
+        AuthResponse authResponse = authService.login(request);
+        cookieOperation(authResponse, response);
+        return "redirect:/pages/home";
+    }
+
+    private void cookieOperation(AuthResponse authResponse, HttpServletResponse response) {
+        Cookie tokenCookie = new Cookie("access_token", authResponse.getToken());
+        Cookie idCookie = new Cookie("user_id", authResponse.getId().toString());
+
+        tokenCookie.setMaxAge(24 * 60 * 60);
+        tokenCookie.setPath("/");
+        idCookie.setMaxAge(24 * 60 * 60);
+        idCookie.setPath("/");
+
+        response.addCookie(tokenCookie);
+        response.addCookie(idCookie);
     }
 }
